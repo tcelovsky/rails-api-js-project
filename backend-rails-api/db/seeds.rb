9e1d692a0da474c23c4a921cbc8ca0196779458c
tscotce <tatyana.celovsky@gmail.com>
2021-01-02 10:34:03 -0500

    Add seed data

--- backend-rails-api/db/seeds.rb
+++ backend-rails-api/db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+list_a = List.create(title: "To Do")
+list_b = List.create(title: "Grocery Shopping")
+
+list_item_a = List_item.create(list: list_b, content: "Pick up dry cleaning")
+list_item_b = List_item.create(list: list_b, content: "Clean")
+list_item_c = List_item.create(list: list_b, content: "Finish work project")
+
+list_item_d = List_item.create(list: list_b, content: "Milk")
+list_item_e = List_item.create(list: list_b, content: "Eggs")
+list_item_f = List_item.create(list: list_b, content: "Beans")
