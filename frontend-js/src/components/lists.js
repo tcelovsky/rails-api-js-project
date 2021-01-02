0ea73af45929988c85bb06b4e5dc30239c5dc67d
tscotce <tatyana.celovsky@gmail.com>
2021-01-02 12:54:31 -0500

    Build out lists.js

--- frontend-js/src/components/lists.js
+++ frontend-js/src/components/lists.js
@@ -5,4 +5,50 @@ class Lists {
         this.adapter = new ListsAdapter()
         this.fetchAndLoadLists()
     }
+
+    initBindingsAndEventListeners() {
+        this.listsForm = document.getElementById('new-list-form')
+        this.listsInput = document.getElementById('new-list-item')
+        this.listsNode = document.getElementById('lists-container')
+        this.listsForm.addEventListener('submit',this.handleAddList.bind(this))
+        this.listsNode.addEventListener('click',this.handleDeleteList.bind(this))
+    }
+
+    fetchAndLoadLists() {
+        this.adapter.getLists()
+        .then( listsJSON => listsJSON.forEach( list => this.list.push( new List(list) )))
+          .then( this.render.bind(this) )
+          .catch( (error) => console.log(error) )
+      }
+
+      handleAddList() {
+        event.preventDefault()
+        const body = this.listInput.value
+        this.adapter.createList(content)
+        .then( (listJSON) => this.lists.push(new List(listSON)) )
+        .then(  this.render.bind(this) )
+        .then( () => this.listInput.value = '' )
+      }
+
+      handleDeleteList() {
+        if (event.target.dataset.action === 'delete-list' && event.target.parentElement.classList.contains("list-element")) {
+          const listId = event.target.parentElement.dataset.listid
+          this.adapter.deleteList(listId)
+          .then( resp => this.removeDeletedList(resp) )
+        }
+      }
+
+      removeDeletedList(deleteResponse) {
+        this.lists = this.lists.filter( list => list.id !== deleteResponse.listId )
+        this.render()
+      }
+
+      notesHTML() {
+        return this.lists.map( list => list.render() ).join('')
+      }
+
+      render() {
+        this.listsNode.innerHTML = `<ul>${this.listsHTML()}</ul>`
+      }
 }
+
--- frontend-js/src/index.js
+++ frontend-js/src/index.js
@@ -1,6 +1,1 @@
-const BASE_URL = "http://localhost:3000"
-const LISTS_URL = `${BASE_URL}/lists`
-const LIST_ITEMS_URL = `${BASE_URL}/list_items`
-const main = document.querySelector("main")
-
 const app = new App()
