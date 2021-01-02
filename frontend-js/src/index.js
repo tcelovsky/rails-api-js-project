83cd8bceb8a3cd8c0c439e05dbc39ee8e7a16d4e
tscotce <tatyana.celovsky@gmail.com>
2021-01-02 12:20:19 -0500

    Add app const to index.js

--- frontend-js/src/index.js
+++ frontend-js/src/index.js
@@ -2,3 +2,5 @@ const BASE_URL = "http://localhost:3000"
 const LISTS_URL = `${BASE_URL}/lists`
 const LIST_ITEMS_URL = `${BASE_URL}/list_items`
 const main = document.querySelector("main")
+
+const app = new App()
