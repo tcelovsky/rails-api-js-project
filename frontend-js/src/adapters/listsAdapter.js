5c83229b33b3c62b0a9a165b791712d757ff4930
tscotce <tatyana.celovsky@gmail.com>
2021-01-02 12:31:10 -0500

    Add createList and deleteList methods to listsAdapter.js

--- frontend-js/src/adapters/listsAdapter.js
+++ frontend-js/src/adapters/listsAdapter.js
@@ -11,4 +11,25 @@ class ListsAdapter {
     getLists() {
         return fetch(this.baseUrl).then(res => res.json())
     }
+
+    createList(title) {
+        const listCreateParams = {
+            method: "POST", 
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ title })
+        }
+        return fetch(this.baseUrl, listCreateParams).then(res => res.json())
+    }
+
+    deleteList(listId) {
+        const listDeleteParams = {
+            method: "DELETE",
+            headers: {
+                "Content-type": "application/json"
+            }
+        }
+        return fetch(`${this.baseUrl}/${listId}`, listDeleteParams).then(res => res.json())
+    }
 }
