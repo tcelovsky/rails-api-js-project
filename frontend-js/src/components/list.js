856cefc4c87abd1adb291b7d388b89f09b195b8c
tscotce <tatyana.celovsky@gmail.com>
2021-01-02 13:00:47 -0500

    Build out list.js

--- frontend-js/src/components/list.js
+++ frontend-js/src/components/list.js
@@ -3,4 +3,18 @@ class List {
         this.title = listJSON.title
         this.id = listJSON.id
     }
+
+    renderShow() {
+        return `<h3>${this.title}</h3>`
+    }
+
+    render() {
+        return ```
+        <li data-listid='${this.id}' data-props='${JSON.stringify(this)} class='list-element'>
+            <a class="show-link" href='#'>${this.title}</a> 
+            <button data-action='edit-list'>Edit</button>
+            <i data-action='delete-list'></i>
+        </li>
+        ```
+    }
 }
