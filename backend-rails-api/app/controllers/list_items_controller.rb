32e29abf757fdb2256afc0363636ccbe7185627f
tscotce <tatyana.celovsky@gmail.com>
2021-01-02 11:35:24 -0500

    Add create, update and delete actions to list_items controller

--- backend-rails-api/app/controllers/list_items_controller.rb
+++ backend-rails-api/app/controllers/list_items_controller.rb
@@ -8,4 +8,26 @@ class ListItemsController < ApplicationController
         list_item = ListItem.find(params[:id])
         render json: list_item
     end
+
+    def create
+        list = List.find(params[:list_id])
+        list_item = list.list_item.build(list_item_params)
+        render json: list_item.save ? list_item : {message: list_item.errors.messages[0]}
+    end
+
+    def update
+        list = List.find(params[:list_id])
+        list_item = list.list_item.update(list_item_params)
+        render json: list_item
+    end
+
+    def delete
+        list_item = ListItem.find(params[:id])
+        list_item.delete
+    end
+
+    private
+    def list_item_params
+        params.require(:list_item).permit(:content)
+    end
 end
