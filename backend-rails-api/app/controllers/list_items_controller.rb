f40610d53e1599498211729663c365cb55b09174
tscotce <tatyana.celovsky@gmail.com>
2021-01-02 11:27:43 -0500

    Add index and show actions to list_items controller

--- backend-rails-api/app/controllers/list_items_controller.rb
+++ backend-rails-api/app/controllers/list_items_controller.rb
@@ -1,2 +1,11 @@
 class ListItemsController < ApplicationController
+    def index 
+        list_items = ListItem.all
+        render json: list_items
+    end
+
+    def show
+        list_item = ListItem.find(params[:id])
+        render json: list_item
+    end
 end
