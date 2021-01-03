89fa060811057bd64c909cafffb41063196bae1d
tscotce <tatyana.celovsky@gmail.com>
2021-01-03 17:47:21 -0500

    Update event listener for DOM content load

--- frontend-js/src/adapters/listsAdapter.js
+++ frontend-js/src/adapters/listsAdapter.js
@@ -3,35 +3,37 @@ const LISTS_URL = `${BASE_URL}/lists`
 const LIST_ITEMS_URL = `${BASE_URL}/list_items`
 const main = document.querySelector("main")
 
+document.addEventListener("DOMContentLoaded", () => ListsAdapter)
+
 class ListsAdapter {
     constructor() {
         this.baseUrl = LISTS_URL
     }
 
-    document.addEventListener("DOMContentLoaded", () => loadLists())
-
     loadLists() {
-        return fetch(this.baseUrl).then(res => res.json())
+        return fetch(this.baseUrl).then(res => res.json()).then(json => {
+            json.forEach(list => renderList(list))
+        })
     }
 
-    createList(title) {
-        const listCreateParams = {
-            method: "POST", 
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({ title })
-        }
-        return fetch(this.baseUrl, listCreateParams).then(res => res.json())
-    }
+    // createList(title) {
+    //     const listCreateParams = {
+    //         method: "POST", 
+    //         headers: {
+    //             "Content-Type": "application/json"
+    //         },
+    //         body: JSON.stringify({ title })
+    //     }
+    //     return fetch(this.baseUrl, listCreateParams).then(res => res.json())
+    // }
 
-    deleteList(listId) {
-        const listDeleteParams = {
-            method: "DELETE",
-            headers: {
-                "Content-type": "application/json"
-            }
-        }
-        return fetch(`${this.baseUrl}/${listId}`, listDeleteParams).then(res => res.json())
-    }
+    // deleteList(listId) {
+    //     const listDeleteParams = {
+    //         method: "DELETE",
+    //         headers: {
+    //             "Content-type": "application/json"
+    //         }
+    //     }
+    //     return fetch(`${this.baseUrl}/${listId}`, listDeleteParams).then(res => res.json())
+    // }
 }
