fabac68d8954397c763deca46f6d7680365b4996
tscotce <tatyana.celovsky@gmail.com>
2021-01-03 17:51:06 -0500

    Update lists.js to load list items

--- frontend-js/src/components/list.js
+++ frontend-js/src/components/list.js
@@ -1,20 +1,21 @@
-// class List {
-//     constructor(listJSON) {
-//         this.title = listJSON.title
-//         this.id = listJSON.id
-//     }
+class List {
+    constructor(listJSON) {
+        this.title = listJSON.title
+        this.id = listJSON.id
+        this.listItems = listJSON.list_items
+    }
 
-//     renderShow() {
-//         return `<h3>${this.title}</h3>`
-//     }
+    renderShow() {
+        return `<h3>${this.title}</h3>`
+    }
 
-//     render() {
-//         return ```
-//         <li data-listid='${this.id}' data-props='${JSON.stringify(this)} class='list-element'>
-//             <a class="show-link" href='#'>${this.title}</a> 
-//             <button data-action='edit-list'>Edit</button>
-//             <i data-action='delete-list'></i>
-//         </li>
-//         ```
-//     }
-// }
+    render() {
+        return ```
+        <li data-listid='${this.id}' data-props='${JSON.stringify(this)} class='list-element'>
+            <a class="show-link" href='#'>${this.title}</a> 
+            <button data-action='edit-list'>Edit</button>
+            <i data-action='delete-list'></i>
+        </li>
+        ```
+    }
+}
