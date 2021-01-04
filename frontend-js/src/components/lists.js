bd96c4082def542e95eaa0f3807ce2d761362454
tscotce <tatyana.celovsky@gmail.com>
2021-01-03 20:00:37 -0500

    Building out renderListItem

--- frontend-js/src/components/lists.js
+++ frontend-js/src/components/lists.js
@@ -1,5 +1,5 @@
 const main = document.querySelector("main")
-const listsContainer = document.querySelector("#lists-container")
+const listContainer = document.querySelector("#list-container")
 
 class Lists {
     constructor() {
@@ -25,52 +25,22 @@ class Lists {
 
     renderList(list) {
         console.log(list)
-        listsContainer.setAttribute("data-id", list.id)
+        listContainer.setAttribute("data-id", list.id)
         const h3 = document.createElement("h3")
-        const ul = document.createElement("ul")
 
         h3.innerText = list.title
-        list.list_items.forEach(list_item => renderListItem(list_item))
-        listsContainer.appendChild(h3)
+        list.list_items.forEach(list_item => this.renderListItem(list_item))
+        listContainer.appendChild(h3)
     }
 
     renderListItem(list_item) {
-        const ul
-        const ul = document.createElement("li")
+        const ul = document.createElement("ul")
+        const li = document.createElement("li")
+        li.setAttribute("data-id", list_item.id)
+
+        li.innerText = list_item.content
+        ul.appendChild(li)
+        h3.appendChild(ul)
     }
-
-    //     .then( listsJSON => listsJSON.forEach( list => this.list.push( new List(list) )))
-    //       .then( this.render.bind(this) )
-    //       .catch( (error) => console.log(error) )
-
-    //   handleAddList() {
-    //     event.preventDefault()
-    //     const body = this.listInput.value
-    //     this.adapter.createList(content)
-    //     .then( (listJSON) => this.lists.push(new List(listSON)) )
-    //     .then(  this.render.bind(this) )
-    //     .then( () => this.listInput.value = '' )
-    //   }
-
-    //   handleDeleteList() {
-    //     if (event.target.dataset.action === 'delete-list' && event.target.parentElement.classList.contains("list-element")) {
-    //       const listId = event.target.parentElement.dataset.listid
-    //       this.adapter.deleteList(listId)
-    //       .then( resp => this.removeDeletedList(resp) )
-    //     }
-    //   }
-
-    //   removeDeletedList(deleteResponse) {
-    //     this.lists = this.lists.filter( list => list.id !== deleteResponse.listId )
-    //     this.render()
-    //   }
-
-    //   notesHTML() {
-    //     return this.lists.map( list => list.render() ).join('')
-    //   }
-
-    //   render() {
-    //     this.listsNode.innerHTML = `<ul>${this.listsHTML()}</ul>`
-    //   }
 }
 
