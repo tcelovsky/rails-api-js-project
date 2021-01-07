663b6cb1d0134efee161ebb36cf3f0ba67d8e75d
tscotce <tatyana.celovsky@gmail.com>
2021-01-07 18:17:06 -0500

    Move render list logic to list.js

--- frontend-js/src/components/list.js
+++ frontend-js/src/components/list.js
@@ -1,5 +1,38 @@
 class List {
     constructor(listJSON) {
         this.title = listJSON.title
+        this.id = listJSON.id
+    }
+
+    renderList() {
+        const listsContainer = document.querySelector("#lists-container")
+
+        const div = document.createElement("div")
+        const h3 = document.createElement("h3")
+        const ul = document.createElement("ul")
+        const form = document.createElement("form")
+        const inputText = document.createElement("input")
+        const inputButton = document.createElement("input")
+        
+        div.setAttribute("id", "list-container")
+        div.setAttribute("data-list-id", this.id)
+        h3.setAttribute("title-list-id", this.id)
+        ul.setAttribute("data-list-id", this.id)
+        form.setAttribute("id", "new-list-item")
+        inputText.setAttribute("type", "text")
+        inputText.setAttribute("name", "list-item")
+        inputText.setAttribute("id", "new-list-item-input")
+        inputButton.setAttribute("type", "submit")
+        inputButton.setAttribute("value", "Add List Item")
+        // inputButton.addEventListener('submit', this.addListItem)
+
+        h3.innerText = this.title
+
+        form.appendChild(inputText)
+        form.appendChild(inputButton)
+        div.appendChild(h3)
+        div.appendChild(ul)
+        div.appendChild(form)
+        listsContainer.appendChild(div)
     }
 }
--- frontend-js/src/components/lists.js
+++ frontend-js/src/components/lists.js
@@ -13,52 +13,58 @@ class Lists {
 
     loadLists() {
         this.adapter.getLists().then(lists => {
-            lists.forEach(list => this.lists.push(new List(list)))
+            lists.forEach(list => this.lists.push(new List(list))
+            )
+        })
+        .then(() => {
+            this.renderLists()
         })
     }
 
-    renderList(list) {
-        const div = document.createElement("div")
-        const h3 = document.createElement("h3")
-        const ul = document.createElement("ul")
-        const form = document.createElement("form")
-        const inputText = document.createElement("input")
-        const inputButton = document.createElement("input")
+    renderLists() {
+        this.lists.map(list => list.renderList()).join('')
+
+        // const div = document.createElement("div")
+        // const h3 = document.createElement("h3")
+        // const ul = document.createElement("ul")
+        // const form = document.createElement("form")
+        // const inputText = document.createElement("input")
+        // const inputButton = document.createElement("input")
         
-        div.setAttribute("id", "list-container")
-        div.setAttribute("data-list-id", list.id)
-        h3.setAttribute("title-list-id", list.id)
-        ul.setAttribute("data-list-id", list.id)
-        form.setAttribute("id", "new-list-item")
-        inputText.setAttribute("type", "text")
-        inputText.setAttribute("name", "list-item")
-        inputText.setAttribute("id", "new-list-item-input")
-        inputButton.setAttribute("type", "submit")
-        inputButton.setAttribute("value", "Add List Item")
-        inputButton.addEventListener('submit', this.addListItem)
+        // div.setAttribute("id", "list-container")
+        // div.setAttribute("data-list-id", list.id)
+        // h3.setAttribute("title-list-id", list.id)
+        // ul.setAttribute("data-list-id", list.id)
+        // form.setAttribute("id", "new-list-item")
+        // inputText.setAttribute("type", "text")
+        // inputText.setAttribute("name", "list-item")
+        // inputText.setAttribute("id", "new-list-item-input")
+        // inputButton.setAttribute("type", "submit")
+        // inputButton.setAttribute("value", "Add List Item")
+        // inputButton.addEventListener('submit', this.addListItem)
 
-        h3.innerText = list.title
+        // h3.innerText = list.title
 
-        form.appendChild(inputText)
-        form.appendChild(inputButton)
-        div.appendChild(h3)
-        div.appendChild(ul)
-        div.appendChild(form)
-        listsContainer.appendChild(div)
+        // form.appendChild(inputText)
+        // form.appendChild(inputButton)
+        // div.appendChild(h3)
+        // div.appendChild(ul)
+        // div.appendChild(form)
+        // listsContainer.appendChild(div)
 
-        list.list_items.forEach(list_item => this.renderListItem(list_item))
+        // list.list_items.forEach(list_item => this.renderListItem(list_item))
     }
 
-    renderListItem(list_item) {
-        const ul = document.querySelector(`ul[data-list-id="${list_item.list_id}"]`)
+    // renderListItem(list_item) {
+    //     const ul = document.querySelector(`ul[data-list-id="${list_item.list_id}"]`)
 
-        const li = document.createElement("li")
-        li.setAttribute("data-list_item-id", list_item.id)
+    //     const li = document.createElement("li")
+    //     li.setAttribute("data-list_item-id", list_item.id)
 
-        li.innerText = list_item.content
+    //     li.innerText = list_item.content
 
-        ul.appendChild(li)        
-    }
+    //     ul.appendChild(li)        
+    // }
 
     // addListItem(e) {
     //     e.preventDefault() //regular behavior will be prevented, no refreshing of the page
