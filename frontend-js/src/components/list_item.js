0759cb8a2efc6d7e6bcacc32ae78ab22e500143c
tscotce <tatyana.celovsky@gmail.com>
2021-01-07 20:29:08 -0500

    Add renderListItems method

--- frontend-js/src/components/list_item.js
+++ frontend-js/src/components/list_item.js
@@ -3,4 +3,16 @@ class ListItem {
         this.id = listItemJSON.id
         this.content = listItemJSON.content
         this.listId = listItemJSON.list_id
+    }
+
+    renderListItem() {
+        const ul = document.querySelector(`ul[data-list-id="${this.listId}"]`)
+
+        const li = document.createElement("li")
+        li.setAttribute("data-list_item-id", this.id)
+
+        li.innerText = this.content
+
+        ul.appendChild(li) 
+    }
 }
--- frontend-js/src/components/lists.js
+++ frontend-js/src/components/lists.js
@@ -18,24 +18,21 @@ class Lists {
         .then(() => {
             this.renderLists()
         })
+        .then(() => {
+            this.renderListItems()
+        })
     }
 
     renderLists() {
         this.lists.map(list => list.renderList()).join('')
-        console.log(this.lists)
-        // list.list_items.forEach(list_item => this.renderListItem(list_item))
     }
 
-    // renderListItem(list_item) {
-    //     const ul = document.querySelector(`ul[data-list-id="${list_item.list_id}"]`)
-
-    //     const li = document.createElement("li")
-    //     li.setAttribute("data-list_item-id", list_item.id)
-
-    //     li.innerText = list_item.content
-
-    //     ul.appendChild(li)        
-    // }
+    renderListItems() {
+        const listItems = []
+        this.lists.forEach(list => {
+            list.listItems.forEach(listItem => listItems.push(listItem))
+        })
+    }
 
     // addListItem(e) {
     //     e.preventDefault() //regular behavior will be prevented, no refreshing of the page
