7dc89681d9d6b0475143ae637b99835192de21fe
tscotce <tatyana.celovsky@gmail.com>
2021-02-01 17:55:50 -0500

    Add event listener to list container to listen for a click

--- frontend-js/src/components/list.js
+++ frontend-js/src/components/list.js
@@ -21,6 +21,7 @@ class List {
         
         div.setAttribute("id", "list-container")
         div.setAttribute("data-list-id", this.id)
+        div.addEventListener('click', this.editListItem.bind(this))
         h3.setAttribute("title-list-id", this.id)
         ul.setAttribute("data-list-id", this.id)
         form.setAttribute("id", "new-list-item")
@@ -59,5 +60,11 @@ class List {
         this.adapter.createListItem(newListItem)
         .then(json => new ListItem (json))
         newListItemInput.value = ''
+    }
+
+    editListItem(e) {
+        const li = e.target
+        li.contentEditable = true
+        li.focus
     }
 }
