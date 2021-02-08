1f2f2f9f284641c7bda4aeb79cccdb66a7689876
tscotce <tatyana.celovsky@gmail.com>
2021-02-08 17:10:51 -0500

    Working on delete list item button

--- frontend-js/src/components/listItem.js
+++ frontend-js/src/components/listItem.js
@@ -19,24 +19,13 @@ class ListItem {
         deleteButton.setAttribute("button-list-id", this.id)
         deleteButton.setAttribute("type", "submit")
         deleteButton.setAttribute("value", "Delete List Item")
-        // deleteButton.addEventListener('click', this.deleteListItem.bind(this))
+        deleteButton.addEventListener('click', this.deleteListItem.bind(this))
 
         li.appendChild(deleteButton)
         ul.appendChild(li) 
     }
 
-    // editListItem(e) {
-    //     e.stopImmediatePropagation()
-    //     const li = document.querySelector(`li[data-list_item_id="${this.id}"]`)
-    //     li.contentEditable = true
-    //     li.focus
-    //     const newListItemValue = li.innerHTML
-    //     console.log(newListItemValue)
-    //     const id = li.dataset.list_item_id
-    //     const updatedListItem = {
-    //         id: id,
-    //         content: newListItemValue
-    //     }
-    //     // document.onclick = this.adapter.updateListItem(updatedListItem)
-    // }
+    deleteListItem() {
+        console.log('delete button')
+    }
 }
