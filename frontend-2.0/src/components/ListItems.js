4bc089074020f114998337392bad1855f15f267f
tscotce <tatyana.celovsky@gmail.com>
2021-05-06 19:50:19 -0400

    Update props

--- frontend-2.0/src/components/ListItems.js
+++ frontend-2.0/src/components/ListItems.js
@@ -4,7 +4,7 @@ import ListItem from './ListItem';
 class ListItems extends Component {
     generateListItems = () => {
         return this.props.items.map(item => 
-            <ListItem key={item.list_id} content={item.content} />
+            <ListItem key={item.id} item={item} />
         )
     }
 
