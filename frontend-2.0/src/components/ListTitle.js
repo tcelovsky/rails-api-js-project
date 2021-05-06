ffc4de26e1b6e07abe45b0e4650f92c6e14ed5d7
tscotce <tatyana.celovsky@gmail.com>
2021-05-06 18:26:39 -0400

    Create basic ListTitle component

---
+++ frontend-2.0/src/components/ListTitle.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ListTitle extends Component {
+
+    render() {
+        return (
+            <div className="list-title">
+            </div>
+        )
+    }
+}
+
+export default ListTitle;
