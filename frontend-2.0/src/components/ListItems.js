dfac4e49cd8d4e9a21a36c1743d3b7034e1e3a75
tscotce <tatyana.celovsky@gmail.com>
2021-05-06 19:17:49 -0400

    Update ListItems

--- frontend-2.0/src/components/ListItems.js
+++ frontend-2.0/src/components/ListItems.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 
-class ListItem extends Component {
+class ListItems extends Component {
 
     render() {
         return (
             <div className="list-item">
+                {console.log(this.props.items)}
             </div>
         )
     }
 }
 
-export default ListItem;
+export default ListItems;
