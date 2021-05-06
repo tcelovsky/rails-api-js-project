75c99f87e31b92615d070f6f8f0d24f67a033431
tscotce <tatyana.celovsky@gmail.com>
2021-05-06 18:27:06 -0400

    Create basic ListItem component

---
+++ frontend-2.0/src/components/ListItem.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ListItem extends Component {
+
+    render() {
+        return (
+            <div className="list-item">
+            </div>
+        )
+    }
+}
+
+export default ListItem;
