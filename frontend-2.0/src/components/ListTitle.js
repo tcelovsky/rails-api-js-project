2bc6daea1693c0a13caee86690e89ae87dbc3b10
tscotce <tatyana.celovsky@gmail.com>
2021-05-06 18:37:14 -0400

    Append list titles

--- frontend-2.0/src/components/ListTitle.js
+++ frontend-2.0/src/components/ListTitle.js
@@ -5,6 +5,7 @@ class ListTitle extends Component {
     render() {
         return (
             <div className="list-title">
+                <h3>{this.props.title}</h3>
             </div>
         )
     }
