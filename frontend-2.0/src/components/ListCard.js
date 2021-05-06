731e2742cadf0b2d4b12884d3717e9a36f7822a2
tscotce <tatyana.celovsky@gmail.com>
2021-05-06 18:33:37 -0400

    Pass props

--- frontend-2.0/src/components/ListCard.js
+++ frontend-2.0/src/components/ListCard.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import ListTitle from './ListTitle';
+import ListItem from './ListItem';
 
 class ListCard extends Component {
     
     render() {
         return (
             <div className="list-card" id={this.props.list.id}>
+                <ListTitle title={this.props.list.title}/>
+                <ListItem items={this.props.list.list_items}/>
             </div>
         )
     }
