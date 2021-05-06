cfbde0247aeb50537b435201a1d24b8628d3d02a
tscotce <tatyana.celovsky@gmail.com>
2021-05-06 19:45:36 -0400

    Import ListItem into ListItems

--- frontend-2.0/src/components/ListItems.js
+++ frontend-2.0/src/components/ListItems.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
+import ListItem from './ListItem';
 
 class ListItems extends Component {
+    generateListItems = () => {
+        return this.props.items.map(item => 
+            <ListItem key={item.list_id} content={item.content} />
+        )
+    }
 
     render() {
         return (
-            <div className="list-item">
-                {console.log(this.props.items)}
+            <div className="list-items-container">
+                {this.generateListItems()}
             </div>
         )
     }
