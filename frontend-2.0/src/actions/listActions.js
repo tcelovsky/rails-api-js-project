e46b92539773a1aa83827fbbb20a90aef4311197
tscotce <tatyana.celovsky@gmail.com>
2021-05-07 12:45:53 -0400

    Refactor fetchLists to listActions ;

--- frontend-2.0/src/actions/listActions.js
+++ frontend-2.0/src/actions/listActions.js
@@ -1,19 +1,6 @@
 const BASE_URL = "http://localhost:3000"
 const LISTS_URL = `${BASE_URL}/lists`
 
-export const fetchLists = () => {
-  return (dispatch) => {
-    dispatch({ type: 'LOADING_LISTS'})
-    fetch(LISTS_URL)
-      .then(response => {
-        return response.json()
-      })
-      .then(responseJSON => {
-        dispatch({ type: 'ADD_LISTS', lists: responseJSON })
-      })
-  }
-}
-
 export const addList = (list) => {
   const data = {
     title: list.title
