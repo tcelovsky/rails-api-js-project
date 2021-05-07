b47ac55faa963bd88600998618ea2f665acf1583
tscotce <tatyana.celovsky@gmail.com>
2021-05-07 12:45:01 -0400

    Add addList functionality

--- frontend-2.0/src/components/AddList.js
+++ frontend-2.0/src/components/AddList.js
@@ -1,12 +1,35 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
+import { addList } from '../actions/listActions';
 
 class AddList extends Component {
+    constructor() {
+        super();
+        this.state = {
+          list: {
+              title: ''
+          }
+        };
+      }
+
+    handleChange = e => {
+        this.setState({
+            list: {
+                title: e.target.value
+            }
+        })
+    }
+
+    handleSubmit = e => {
+        e.preventDefault();
+        addList(this.state.list)
+    }
 
     render() {
         return (
             <div className="new-list-container">
-                <form>
-                    <input type="text"></input>
+                <form onSubmit={ e => this.handleSubmit(e) }>
+                    <input type="text" onChange={this.handleChange} value={this.state.list.title}></input>
                     <input type="submit" value="Add List"></input>
                 </form>
             </div>
