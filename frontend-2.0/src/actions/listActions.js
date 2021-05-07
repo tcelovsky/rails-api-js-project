f128c601ed64b2a7077e4fa712d8a4596d14aa6a
tscotce <tatyana.celovsky@gmail.com>
2021-05-07 12:44:21 -0400

    Add addList action

--- frontend-2.0/src/actions/listActions.js
+++ frontend-2.0/src/actions/listActions.js
@@ -2,14 +2,31 @@ const BASE_URL = "http://localhost:3000"
 const LISTS_URL = `${BASE_URL}/lists`
 
 export const fetchLists = () => {
-    return (dispatch) => {
-      dispatch({ type: 'LOADING_LISTS'})
-      fetch(LISTS_URL)
-        .then(response => {
-          return response.json()
-        })
-        .then(responseJSON => {
-          dispatch({ type: 'ADD_LISTS', lists: responseJSON })
-        })
-    }
+  return (dispatch) => {
+    dispatch({ type: 'LOADING_LISTS'})
+    fetch(LISTS_URL)
+      .then(response => {
+        return response.json()
+      })
+      .then(responseJSON => {
+        dispatch({ type: 'ADD_LISTS', lists: responseJSON })
+      })
   }
+}
+
+export const addList = (list) => {
+  const data = {
+    title: list.title
+  }
+  const configObj = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    },
+    body: JSON.stringify(data)
+  }
+  return fetch(LISTS_URL, configObj)
+    .then(response => response.json())
+}
+
