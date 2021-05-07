845874718e4505ee86fa255ea64ac71eab793ff7
tscotce <tatyana.celovsky@gmail.com>
2021-05-06 20:18:09 -0400

    Import Header into App

--- frontend-2.0/src/containers/App.js
+++ frontend-2.0/src/containers/App.js
@@ -5,6 +5,7 @@ import { connect } from 'react-redux';
 import { fetchLists } from '../actions/listActions';
 import Lists from '../components/Lists';
 import Header from '../components/Header';
+import Navbar from '../components/Navbar';
 
 class App extends Component {
 
@@ -15,6 +16,7 @@ class App extends Component {
   render() {
     return (
         <div className="App">
+          <Navbar />
           <Header />
           <Switch>
             {/* <Route exact path='/' render={() => <Link to={'/lists'}><Home /></Link>} /> */}
