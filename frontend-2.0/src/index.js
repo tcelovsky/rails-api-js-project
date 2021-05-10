7560d41ccb3e928826d064c2d54123b504563fe4
tscotce <tatyana.celovsky@gmail.com>
2021-05-10 16:54:01 -0400

    Remove css import

--- frontend-2.0/src/index.js
+++ frontend-2.0/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import './styles/index.css';
 import App from './containers/App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from 'react-router-dom';
