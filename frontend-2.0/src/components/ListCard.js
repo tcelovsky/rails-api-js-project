8da6378644877cfb7993f9b3c0e02a7352d64196
tscotce <tatyana.celovsky@gmail.com>
2021-05-12 18:27:36 -0400

    Create basic edit list button

--- frontend-2.0/src/components/ListCard.js
+++ frontend-2.0/src/components/ListCard.js
@@ -3,12 +3,14 @@ import ListTitle from './ListTitle';
 import Items from './Items';
 import DeleteListButton from '../containers/DeleteListButton';
 import AddItem from '../containers/AddItem';
+import EditListButton from './EditListButton';
 
 class ListCard extends Component {
 
     render() {
         return (
             <div className="list-card" id={this.props.list.id}>
+                <EditListButton isEditable={this.isEditable}/>
                 <DeleteListButton id={this.props.list.id}/>
                 <ListTitle title={this.props.list.title}/>
                 <Items items={this.props.list.list_items}/>
