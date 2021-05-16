32b46a1d1535683affcbdd7e5a2ab5fc304c07fc
tscotce <tatyana.celovsky@gmail.com>
2021-05-16 12:49:10 -0400

    Create item reducer

--- frontend-2.0/src/reducers/itemReducer.js
+++ frontend-2.0/src/reducers/itemReducer.js
@@ -1,16 +1,11 @@
 export const itemReducer = (state = {
-    items: [],
-    loading: false
+    items: []
 }, action) => {
     switch(action.type) {
-        // case 'EDIT_ITEM':
-        //     return {
-        //         ...state, items: [...state.items], loading: true
-        //     }
-        // case 'DELETE_ITEM':
-        //     return {
-        //         ...state, items: action.items, loading: false
-        //     }
+        case 'EDIT_ITEM':
+            return {
+                ...state, items: [...state.items]
+            }
         default: 
             return state;
     }
