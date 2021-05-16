f59d02d79baf510fddb2ed1f9a09a98802d0c7a0
tscotce <tatyana.celovsky@gmail.com>
2021-05-16 12:50:45 -0400

    Add editItem dispatch to Item.js

--- frontend-2.0/src/containers/Item.js
+++ frontend-2.0/src/containers/Item.js
@@ -2,13 +2,17 @@ import React, { Component } from 'react';
 import DeleteItemButton from './DeleteItemButton';
 import EditItemButton from '../components/EditItemButton';
 import { editItem } from '../actions/itemActions';
-import { fetchLists } from '../actions/listsActions';
 import { connect } from 'react-redux';
 
 class Item extends Component {
     constructor(props) {
         super(props)
         this.state = {
+            item: {
+                content: this.props.item.content,
+                id: this.props.item.id,
+                list_id: this.props.item.list_id
+            },
             isEditable: false
         }
     }
@@ -21,21 +25,30 @@ class Item extends Component {
 
     handleChange = (e) => {
         this.setState({
-            item: {
+            updatedItem: {
                 content: e.target.value,
-                id: this.props.item.id,
-                list_id: this.props.item.list_id
+                id: this.state.item.id,
+                list_id: this.state.item.list_id
             }
         })
     }
 
     handleSubmit = (e) => {
         e.preventDefault();
-        editItem(this.state.item)
-            .then(json => this.props.fetchLists(json))
-            .then(this.setState({
-                isEditable: false
-            }))
+        this.props.editItem(this.state.updatedItem)
+        this.setState({
+            item: {
+                content: this.state.updatedItem.content,
+                id: this.state.updatedItem.id,
+                list_id: this.state.updatedItem.list_id
+            },
+            updatedItem: {
+                content: '',
+                id: '',
+                list_id: ''
+            },
+            isEditable: false
+        })
     }
 
     render() {
@@ -43,16 +56,16 @@ class Item extends Component {
             return (
                 <div className="edit-item-container">
                     <form>
-                        <input type="text" onChange={this.handleChange} defaultValue={this.props.item.content}></input>
+                        <input type="text" onChange={this.handleChange} defaultValue={this.state.item.content}></input>
                         <input type="submit" value="Save" onClick={e => this.handleSubmit(e)}></input>
                     </form>
                 </div>
             )
         } else {
             return (
-                <li className="list-item" id={this.props.item.id}>{this.props.item.content}
+                <li className="list-item" id={this.state.item.id}>{this.state.item.content}
                     <div className="buttons-container">
-                        <DeleteItemButton id={this.props.item.id} />
+                        <DeleteItemButton id={this.state.item.id} />
                         <EditItemButton isEditable={this.isEditable} />
                     </div>
                 </li>
@@ -62,10 +75,16 @@ class Item extends Component {
     }
 }
 
-const mapDispatchToProps = dispatch => {
+const mapStateToProps = state => {
     return {
-        fetchLists: () => dispatch(fetchLists())
+        items: state.item
     }
 }
 
-export default connect(null, mapDispatchToProps)(Item);
+const mapDispatchToProps = dispatch => {
+    return {
+      editItem: updatedItem => dispatch(editItem(updatedItem))
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Item);
